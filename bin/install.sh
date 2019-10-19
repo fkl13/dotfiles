09f0fc2194dee2486da4492ed880eb03aeb8105e
Fabian Klaiber <fklaiber@posteo.net>
2019-10-19 15:03:12 +0200

    Add go, rust and icdiff to install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 add_rpmfusion() {
-        sudo dnf install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+        sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 }
 
 base() {
@@ -74,18 +74,56 @@ install_misc() {
         sudo dnf autoremove -y
 }
 
+# install custom scripts
+install_scripts() {
+        # install icdiff
+        curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
+        curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
+        chmod +x /usr/local/bin/icdiff
+        chmod +x /usr/local/bin/git-icdiff
+}
+
 # install rust
 install_rust() {
         curl https://sh.rustup.rs -sSf | sh
 }
 
+# based on https://github.com/jessfraz/dotfiles/blob/master/bin/install.sh
+install_golang() {
+        export GO_VERSION
+        GO_VERSION=$(curl -sSL "https://golang.org/VERSION?m=text")
+        export GO_SRC=/usr/local/go
+
+        # if we are passing the version
+        if [[ -n "$1" ]]; then
+                GO_VERSION=$1
+        fi
+
+        # purge old src
+        if [[ -d "$GO_SRC" ]]; then
+                sudo rm -rf "$GO_SRC"
+                sudo rm -rf "$GOPATH"
+        fi
+
+        GO_VERSION=${GO_VERSION#go}
+
+        # subshell
+        (
+        kernel=$(uname -s | tr '[:upper:]' '[:lower:]')
+        curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.${kernel}-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
+        )
+}
+
 usage() {
         echo -e "install.sh\\n\\tThis script install my basic setup for fedora\\n"
         echo "Usage:"
-        echo "base      - install base packages"
-        echo "full      - install base, wm and desktop"
-        echo "wm        - install window manager"
-        echo "misc      - install desktop apps"
+        echo "  base      - install base packages"
+        echo "  full      - install base, wm and desktop"
+        echo "  wm        - install window manager"
+        echo "  misc      - install desktop pkgs"
+        echo "  rust      - install rust"
+        echo "  golang    - install go"
+        echo "  scripts   - install scripts"
 }
 
 main() {
@@ -94,16 +132,25 @@ main() {
         if [[ -z "$arg" ]]; then
                 usage
                 exit 1
-        elif [[ $arg = "base" ]]; then
+        fi
+
+        if [[ $arg == "base" ]]; then
                 base
-        elif [[ $arg = "full" ]]; then
+        elif [[ $arg == "full" ]]; then
                 base
                 install_misc
                 install_wm
-        elif [[ $arg = "wm" ]]; then
+                install_scripts
+        elif [[ $arg == "wm" ]]; then
                 install_wm
-        elif [[ $arg = "misc" ]]; then
+        elif [[ $arg == "misc" ]]; then
                 install_misc
+        elif [[ $arg == "rust" ]]; then
+                install_rust
+        elif [[ $arg == "golang" ]]; then
+                install_golang
+        elif [[ $arg == "scripts" ]]; then
+                install_scripts
         else
                 usage
         fi
