d66d2a9d9aab84415ad5304c278b79f7bd4b95e6
Fabian Klaiber <fklaiber@posteo.net>
2021-12-22 15:22:45 +0100

    Install go tools

--- bin/install.sh
+++ bin/install.sh
@@ -123,6 +123,32 @@ install_golang() {
         kernel=$(uname -s | tr '[:upper:]' '[:lower:]')
         curl -sSL "https://storage.googleapis.com/golang/go${GO_VERSION}.${kernel}-amd64.tar.gz" | sudo tar -v -C /usr/local -xz
         )
+
+	# get tools
+	(
+	set -x
+	set +e
+	# tools for vim-go
+	go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
+	go install github.com/go-delve/delve/cmd/dlv@latest
+	go install github.com/kisielk/errcheck@latest
+	go install github.com/davidrjenni/reftools/cmd/fillstruct@master
+	go install github.com/rogpeppe/godef@latest
+	go install golang.org/x/tools/cmd/goimports@master
+	go install golang.org/x/lint/golint@master
+	go install github.com/mgechev/revive@latest
+	go install golang.org/x/tools/gopls@latest
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+	go install honnef.co/go/tools/cmd/staticcheck@latest
+	go install github.com/fatih/gomodifytags@latest
+	go install golang.org/x/tools/cmd/gorename@master
+	go install github.com/jstemmer/gotags@master
+	go install golang.org/x/tools/cmd/guru@master
+	go install github.com/josharian/impl@master
+	go install honnef.co/go/tools/cmd/keyify@master
+	go install github.com/fatih/motion@latest
+	go install github.com/koron/iferr@master
+	)
 }
 
 usage() {
